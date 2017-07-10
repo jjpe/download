27c7705e305e83b61e7482aac6dfd8618b15efe4
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-07-10 09:04:52 +0200

    Implement main functionality

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,11 @@
 extern crate clap;
 extern crate download;
 
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{App, Arg, ArgMatches};
 use download::*;
-use std::env;
-use std::fs::{DirEntry, File};
-use std::io;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process;
 use std::str;
-
 
 const BIN_NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str =  env!("CARGO_PKG_VERSION");
@@ -21,8 +16,42 @@ const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
 fn main() {
     let args = CliArgs::get();
+    match args.to.to_str() {
+        None => {
+            println!("ERROR: Specify a valid destination using --to <FILE>");
+            process::exit(-10);
+        },
+        Some(file_name) if file_name.is_empty() => {
+            println!("ERROR: Specify a valid destination using --to <FILE>");
+            process::exit(-10);
+        },
+        Some(_) => {/* continue below */},
+    }
 
-    println!("args: {:#?}", args);
+    let result: DlResult<DlStatus> = download::Dl::new()
+        .overwrite(args.overwrite)
+        .verbose(args.verbose)
+        .download(&args.from, &args.to);
+    match result {
+        Ok(DlStatus::Downloaded{ location, num_bytes }) => {
+            println!("Downloaded {}  ({} bytes)",
+                     location.display(),
+                     num_bytes);
+        },
+        Ok(DlStatus::Replaced{ location, num_bytes }) => {
+            println!("Downloaded {} (replacement size: {} bytes)",
+                     location.display(),
+                     num_bytes);
+        },
+        Ok(DlStatus::FileExists(location)) => {
+            println!("File exists: {}", location.display());
+            process::exit(-11);
+        },
+        Err(dl_err) => {
+            println!("Failed to download {}:  {:#?}", args.to.display(), dl_err);
+            process::exit(-12);
+        }
+    }
 }
 
 
@@ -31,29 +60,35 @@ fn main() {
 struct CliArgs {
     from: Url,
     to: PathBuf,
-    force: bool
+    overwrite: bool,
+    verbose: bool,
 }
 
 impl CliArgs {
     pub fn get() -> Self {
         let matches: ArgMatches = Self::get_clap_matches();
-        let url: &str = matches.value_of("url").unwrap_or_else(|| {
-            println!("Error: Must provide an --url");
+        let from: &str = matches.value_of("from").unwrap_or_else(|| {
+            println!("Error: Must provide an URL using --from");
             process::exit(-1);
         });
-        let output: &str = matches.value_of("output").unwrap_or_else(|| {
-            println!("Error: Must provide an --output");
+        let to: &str = matches.value_of("to").unwrap_or_else(|| {
+            println!("Error: Must provide a destination file using --to");
             process::exit(-1);
         });
-        let force: bool = match matches.occurrences_of("force") {
+        let overwrite: bool = match matches.occurrences_of("overwrite") {
+            0 => false,
+            _ => true,
+        };
+        let verbose: bool = match matches.occurrences_of("verbose") {
             0 => false,
             _ => true,
         };
 
         Self {
-            from: Url::parse(url).expect("Invalid url"),
-            to: PathBuf::from(output),
-            force: force
+            from: Url::parse(from).expect("Invalid url"),
+            to: PathBuf::from(to),
+            overwrite: overwrite,
+            verbose: verbose,
         }
     }
 
@@ -62,23 +97,24 @@ impl CliArgs {
             .version(VERSION)
             .about(DESCRIPTION)
             .author(AUTHORS)
-            .arg(Arg::with_name("url")
-                 .short("u")
-                 .long("url")
+            .arg(Arg::with_name("from")
+                 .long("from")
                  .value_name("URL")
                  .help("The URL from where to download")
                  .takes_value(true))
-            .arg(Arg::with_name("output")
-                 .short("-o")
-                 .long("output")
+            .arg(Arg::with_name("to")
+                 .long("to")
                  .value_name("FILE")
-                 .help("The destination location to which to write the downloaded data.")
+                 .help("The file system destination for the downloaded data.")
                  .takes_value(true))
-            .arg(Arg::with_name("force")
-                 .short("f")
-                 .long("force")
-                 .help("If set, overwrite FILE if it existed.
-If not set, display a message if the file existed.
+            .arg(Arg::with_name("overwrite")
+                 .long("overwrite")
+                 .help("If set, overwrite FILE if it existed, no questions asked.
+If not set and the FILE existed, display a message but don't overwrite.
+By default this flag is not set."))
+            .arg(Arg::with_name("verbose")
+                 .long("verbose")
+                 .help("Use verbose output if set, which includes transfer data.
 By default this flag is not set."))
             .get_matches()
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,205 @@
 extern crate curl;
 extern crate url;
+extern crate serde;
+#[macro_use(Deserialize,Serialize)] extern crate serde_derive;
+extern crate serde_json;
 
 use curl::easy::Easy;
-use curl::Error as CurlErr;
-use std::env;
-use std::fs::{DirEntry, File};
+use std::fs::File;
 use std::io;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::process;
 use std::str;
-pub use url::{Url};
+pub use url::Url;
 
+
+
+pub type DlResult<T> = Result<T, DlErr>;
+
+#[derive(Debug)]
+pub enum DlErr {
+    CurlErr(curl::Error),
+    IoErr(io::Error),
+    JsonErr(serde_json::Error),
+
+    DowncastFailure { msg: String }
+}
+
+impl From<curl::Error> for DlErr {
+    fn from(err: curl::Error) -> DlErr {  DlErr::CurlErr(err)  }
+}
+
+impl From<io::Error> for DlErr {
+    fn from(err: io::Error) -> DlErr {  DlErr::IoErr(err)  }
+}
+
+impl From<serde_json::Error> for DlErr {
+    fn from(err: serde_json::Error) -> DlErr {  DlErr::JsonErr(err)  }
+}
+
+
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DlStatus {
+    Downloaded{ location: PathBuf, num_bytes: usize },
+    FileExists(PathBuf),
+    Replaced{ location: PathBuf, num_bytes: usize },
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Dl {
+    buffer: Vec<u8>,
+    overwrite: bool,
+    verbose: bool,
+}
+
+impl Dl {
+    pub fn new() -> Self {
+        Dl {
+            buffer: vec![],
+            overwrite: false,
+            verbose: false,
+        }
+    }
+
+    pub fn overwrite(mut self, overwrite_file: bool) -> Self {
+        self.overwrite = overwrite_file;
+        self
+    }
+
+    pub fn verbose(mut self, verbose: bool) -> Self {
+        self.verbose = verbose;
+        self
+    }
+
+    pub fn download(&mut self, from: &Url, to: &Path) -> DlResult<DlStatus> {
+        self.buffer.clear();
+        let to = PathBuf::from(to);
+        let headers = [
+            // Authorization is handled by the `self.get()` call below,
+            //    and there must not be an "Authorization" header here.
+            format!("Accept: application/octet-stream"),
+            format!("User-Agent: download"),
+        ];
+
+        if to.exists() && self.overwrite {
+            self.get(from, &headers)?;
+            let num_bytes = File::create(&to)?.write(&self.buffer)?;
+            return Ok(DlStatus::Replaced{ location: to, num_bytes: num_bytes });
+        } else if to.exists() && !self.overwrite {
+            // TODO: Inform the user that the download is
+            //    skipped because the file already exists.
+            return Ok(DlStatus::FileExists(to));
+        } else { // `to` does not exist
+            self.get(from, &headers)?;
+            let num_bytes = File::create(&to)?.write(&self.buffer)?;
+            Ok(DlStatus::Downloaded{ location: to, num_bytes: num_bytes })
+        }
+    }
+
+
+    fn get(&mut self, url: &Url, headers: &[String]) -> DlResult<()> {
+        println!("[GET] url: {}", url);
+        let mut easy: Easy = self.configure_easy(url.as_str(), headers)?;
+        easy.get(true)?;
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data: &[u8]| {
+            self.buffer.extend_from_slice(data);
+            Ok(data.len())
+        })?;
+        transfer.perform().map_err(DlErr::CurlErr)
+    }
+
+
+    fn configure_easy(&mut self, url: &str, headers: &[String])
+                      -> DlResult<Easy> {
+        let mut easy = Easy::new();
+        easy.url(url)?;
+        easy.follow_location(true)?;
+        easy.autoreferer(false)?;
+        easy.verbose(self.verbose)?;
+        // easy.username(&self.owner)?;     // TODO:
+        // easy.password(&self.api_token)?; // TODO:
+        easy.progress(true)?;
+
+        let mut header_list = curl::easy::List::new();
+        for header in headers {
+            header_list.append(header)?;
+        }
+        easy.http_headers(header_list)?;
+        Ok(easy)
+    }
+
+
+    // pub fn download(&mut self, release_tag: &str, asset: String) -> DlResult<()> {
+    //     // GET /repos/:owner/:repo/releases/assets/:id
+    //     self.buffer.clear();
+    //     let found_assets: JsonValue = self.list_assets(release_tag)?;
+    //     let value: JsonValue = serde_json::from_slice(&self.buffer)?;
+    //     let asset_path = Path::new(&asset);
+    //     if asset_path.exists() {  return Ok(()); /* Skip the download */  }
+    //     let mut file = File::create(&asset_path)?;
+    //     let downcast_fail = || DlErr::DowncastFailure {
+    //         msg: String::from("asset_obj[\"id\"].as_u64()")
+    //     };
+    //     let mut asset_id: u64 = 0;
+    //     match found_assets {
+    //         JsonValue::Array(ref vec) => {
+    //             for asset_obj in vec.iter().filter(|obj| obj["name"] == asset) {
+    //                 asset_id = asset_obj["id"].as_u64().ok_or_else(downcast_fail)?;
+    //                 break;
+    //             }
+    //         },
+    //         _ => unimplemented!(),
+    //     }
+    //     let url = format!(
+    //         "https://api.github.com/repos/{}/{}/releases/assets/{}",
+    //         self.owner,
+    //         self.repo,
+    //         asset_id
+    //     );
+    //     let headers = [
+    //         // Authorization is handled by the `self.get()` call below,
+    //         //    and there must not be an "Authorization" header here.
+    //         format!("Accept: application/octet-stream"),
+    //         format!("User-Agent: ghdeploy"),
+    //     ];
+    //     self.buffer.clear();
+    //     self.get(&url, &headers)?;
+    //     let num_bytes = file.write(&self.buffer)?;
+    //     println!("Wrote asset @ {}    ({} bytes)", asset, num_bytes);
+    //     Ok(())
+    // }
+
+
+    // fn post(&mut self, url: &str, headers: &[String], file: Option<&File>)
+    //         -> DlResult<()> {
+    //     println!("[POST] url: {}", url);
+    //     let mut easy: Easy = self.configure_easy(url, headers)?;
+    //     easy.upload(true)?;
+    //     easy.post(true)?;
+
+    //     easy.perform()?;
+    //     let mut easy: Easy = self.configure_easy(url, headers)?;
+
+    //     let (bytes, num_bytes) = match file {
+    //         None => (vec![], 0),
+    //         Some(mut file) => {
+    //             let mut bytes = vec![];
+    //             file.read_to_end(&mut bytes)?;
+    //             let len = bytes.len();
+    //             (bytes, len)
+    //         },
+    //     };
+
+    //     easy.in_filesize(num_bytes as u64)?;
+    //     easy.post_field_size(num_bytes as u64)?;
+    //     easy.post_fields_copy(&bytes)?;
+    //     easy.perform().map_err(DlErr::CurlErr)
+    // }
+
+
+}
 
 
 
